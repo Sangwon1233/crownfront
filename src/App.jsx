c80bae40b9aa70c0e3165058634076bd2e4fd075
ihyeonu <dlgusdn2312@gmail.com>
2025-07-08 20:56:17 +0900

    백그라운드

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/layout/Header/Header'; // Header 컴포넌트 임포트
 import Footer from './components/layout/Footer/Footer'; // Footer 컴포넌트 임포트
-import backgroundImage from './assets/backimage.jpg';
 
 
 import './App.css'; // 전역 CSS 파일을 임포트
@@ -13,12 +12,9 @@ function App() {
     <Router>
       <div className="app-wrapper">
         <Header/>
-        <div className="content-wrapper bg-cover bg-center min-h-screen"
-        style={{ backgroundImage: `url(${backgroundImage})` }}>
           <Routes>
             <Route path="/" element={<Home />} />
           </Routes>
-        </div>
       <Footer/>
       </div>
     </Router>
