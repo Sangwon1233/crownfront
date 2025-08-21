97502db3ab14df9a7099f80b7bba833b4c70cadc
Sangwon1233git config --global user.name Sangwon1233git config --global user.name Your <tkddnjs3351@naver.com>
2025-08-21 18:19:41 +0900

    깃허브 아이콘 추가

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+const GitHubIcon = () => (
+  <svg aria-hidden="true" viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
+    <path d="M12 .5a12 12 0 0 0-3.79 23.4c.6.11.82-.26.82-.58v-2.03c-3.34.73-4.04-1.41-4.04-1.41-.55-1.39-1.35-1.77-1.35-1.77-1.1-.74.08-.73.08-.73 1.22.09 1.86 1.26 1.86 1.26 1.08 1.85 2.83 1.32 3.52 1.01.11-.79.42-1.32.76-1.62-2.66-.3-5.47-1.33-5.47-5.93 0-1.31.47-2.38 1.25-3.22-.13-.31-.54-1.56.12-3.25 0 0 1.01-.32 3.3 1.23a11.5 11.5 0 0 1 6 0c2.29-1.55 3.3-1.23 3.3-1.23.66 1.69.25 2.94.12 3.25.78.84 1.25 1.91 1.25 3.22 0 4.61-2.81 5.63-5.49 5.92.43.37.81 1.1.81 2.22v3.29c0 .32.21.7.82.58A12 12 0 0 0 12 .5z"/>
+  </svg>
+);
+
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   
@@ -56,7 +62,7 @@ const Footer = () => {
         <div style={styles.contactInfo}>
           <p>
             <strong>이메일:</strong> 
-            <a href="tkddnjs8097@gmail.com" style={styles.link}>
+            <a href="mailto:tkddnjs8097@gmail.com" style={styles.link}>
               tkddnjs8097@gmail.com
             </a>
           </p>
